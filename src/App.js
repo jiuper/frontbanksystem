d4dc368a309785fff3d584f5200b7cdeb8a27e66
DESKTOP-NT86GV2\jiuper <jiuper52@gmail.com>
2022-07-02 12:17:04 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
 export const  App = () => {
   return (
     <>
-
+  <Routes>
+    <Route path='/*' element={<>home</>}/>
+    <Route path='/login/*' element={<>login</>}/>
+    <Route path='/register/*' element={<>register</>}/>
+    <Route path='/user/*' element={<>user</>}/>
+    <Route path='/score/*' element={<>score</>}/>
+  </Routes>
     </>
   );
 }
